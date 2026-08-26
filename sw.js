ca577e969909a7ef1b3f6f1235c0ed6183fc4521
philipmedler057 <philipmedler057@gmail.com>
2026-08-25 21:31:43 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='issue-report-v8-authscreen-removed';
+const CACHE='issue-report-v8-buttons-fixed';
 const A=['./','./index.html','./manifest.json','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
